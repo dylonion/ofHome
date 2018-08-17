b0eaea823644fa71dca2c3b080d2162bb1421f30
Dylan Moylan <dylanmoylan1@gmail.com>
2018-08-17 14:31:36 -0400

    Edits to allow the about page to display through wordpress.

--- ofhome.js
+++ ofhome.js
@@ -1,6 +1,8 @@
 $(function() {
   $('.story-toggle').on('click', function() {
     console.log('firing')
+    let newText = $(this).siblings('.hidden-story').text()
+    let oldText = $(this).siblings('p').text()
     $(this).siblings('.hidden-story').toggleClass('nodisplay');
   })
 })
