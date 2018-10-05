d6205bf4dc2db4979778f074a9fff71b24b4e933
Dylan Moylan <dylanmoylan1@gmail.com>
2018-10-05 12:45:33 -0400

    Adds the php conversion of the donate page.

---
+++ donate.php
@@ -0,0 +1,133 @@
+<?php /* Template Name: donate */ ?>
+<?php get_header(); ?>
+  <div class="header donate-header">
+    <h1>100%</h1>
+    <h2>
+      of your donations go
+      straight to the people
+      we support.
+    </h2>
+  </div>
+  <div class="content">
+    <article>
+      <h3 class="title-donate">
+        Domestic Violence Survivors
+        + Students
+      </h3>
+      <p>
+        Studies have shown that children who witness domestic violence are twice as likely to
+        abuse their own partners and children in the future. Your donation means that women
+        who are overcoming abuse can receive counseling, financial support, and job training so
+        that their children will have a brighter future.
+      </p>
+      <p>
+        Our students are chosen because they have the drive to succeed. We know that social
+        guidance and financial support for these students will go a long way. Your donation can
+        help change a life.
+      </p>
+    </article>
+  </div>
+  <div class="give">
+  <div class="give-grid">
+    <div class="give-option">
+      <h1>$100</h1>
+      <p>
+        $100 can go towards
+        cell phone and plan for moms
+        or towards tutoring for
+        students.
+      </p>
+      <div class="give-button">Donate Now</div>
+    </div>
+    <div class="give-option">
+      <h1>$250</h1>
+      <p>
+        Money can be used for
+        job training for moms or
+        for much-needed school
+        materials for students.
+      </p>
+      <div class="give-button">Donate Now</div>
+    </div>
+    <div class="give-option">
+      <h1>$500</h1>
+      <p>
+        $500 can go towards a
+        computer, which helps a
+        student in college or
+        towards helping a mom
+        achieve stability at home.
+      </p>
+      <div class="give-button">Donate Now</div>
+    </div>
+    <div class="give-option">
+      <h1>DONATE</h1>
+      <p>
+        Make a donation “in
+        honor of" or "in memory
+        of” someone special.
+      </p>
+      <div class="give-button">Donate Now</div>
+    </div>
+    <div class="give-option">
+      <h1>DONATE</h1>
+      <p>
+        Every dollar makes a
+        difference! Specify how
+        much you want to donate
+        by clicking the button below.
+      </p>
+      <div class="give-button">Donate Now</div>
+      </div>
+    </div>
+  </div>
+  <div class="ways-to-give">
+    <h1>Different Ways to Give</h1>
+    <ul>
+      <li>
+        <h3>COMPANY MATCHING GIFTS</h3>
+        <p>
+          Does your employer have a matching gift program for your monetary donation and/or
+          volunteer service? These programs can increase the impact of your donation to Of
+          Home, Family and Future. Ask your Human Resources Department if these programs
+          are available. Then contact info@ofhome.org for more information.
+        </p>
+      </li>
+      <li>
+        <h3>PAYROLL CAMPAIGNS</h3>
+        <p>
+          Is there a giving campaign at your workplace? Please strongly consider designating your
+          charitable contribution to Of Home, Family and Future.
+        </p>
+      </li>
+      <li>
+        <h3>PLANNED GIVING</h3>
+        <p>
+          Please contact info@ofhome.org to discuss ways to give to Of Home, Family and Future.
+        </p>
+      </li>
+      <li>
+        <h3>SUPPORT US ON AMAZONSMILE</h3>
+        <p>
+          For those of you who shop on Amazon.com, they have an exciting program called
+          AmazonSmile. It’s a simple way for you to support your favorite charity every time you
+          shop, at no cost to you. Once you choose your charity, Amazon automatically donates a
+          small portion of the purchase price on eligible items whenever you place an order.
+        </p>
+        <p>
+          If you’d like to designate Of Home, Family and Future as your charity of choice, which we
+          certainly hope you will do, just go to the following link:
+          <a href="http://smile.amazon.com/ch/20-2014390">http://smile.amazon.com/ch/20-2014390</a>, sign into your Amazon account and then a
+          confirmation should say “Your shopping will support Of Home Family and Future Inc”. It
+          takes less than a minute!
+        </p>
+      </li>
+      <li>
+        <p>
+          Prefer to send a check? Click here for details. Questions? Email: info@ofhome.org
+          Have Questions? Check out our FAQs for more information.
+        </p>
+      </li>
+    </ul>
+  </div>
+<?php get_footer(); ?>
