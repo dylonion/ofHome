fb3002f335bc692fdd3108f146a8fbd19a7c86a8
Dylan Moylan <dylanmoylan1@gmail.com>
2018-11-01 04:00:18 -0400

    Adds php version of the FAQ page.

---
+++ faq.php
@@ -0,0 +1,153 @@
+<?php /* Template Name: FAQ */ ?>
+<?php get_header(); ?>
+  <div class="header faq-header">
+    <h1>FAQ</h1>
+  </div>
+  <div class="content">
+    <ol>
+      <li>
+        <h3>100%: WHAT DOES THAT MEAN?</h3>
+        <article>
+          From the start, we decided that every donation should go straight to the people in our
+          programs. We’re serious when we say 100%. A group of private donors pays for
+          everything including the PayPal and credit card transaction fees we incur anytime you
+          donate online. This is to ensure that every single dollar you give goes straight to the
+          people we help.
+        </article>
+      </li>
+      <li>
+        <h3>GIVING: HOW DO I DONATE?</h3>
+        <article>
+          <section>
+            <h3>ONLINE:</h3>
+            <p>
+              Donating online is our easiest option. You can donate using any major credit card. All
+              international donations made on the Of Home, Family and Future website will be
+              converted to USD. Take care when filling out billing and shipping information; PayPal
+              has a highly sensitive automatic fraud alert system, and it picks up any inconsistencies
+              in these subjects.
+            </p>
+            <p>There are different options for giving online. <a href="">See them here</a>.</p>
+            <p>
+              Security for online donations: We use PayPal, the largest online global payment
+              processor, to securely process all online donations. Don’t worry; you don’t need an
+              account with them to donate to Of Home, Family and Future. And we’ll never share
+              your information with anyone.
+            </p>
+          </section>
+          <section>
+            <h3>BY CHECK:</h3>
+            <p>Send checks (we don’t advise cash) for general donations to our New York office at:</p>
+            <strong>Of Home, Family and Future, Inc.</strong><br />
+            9 East 8th Street, Suite 135<br />
+            New York, NY 10003<br />
+            <p>
+              If you’re donating in honor of someone, or want to direct your donation to something
+              specific, please write this in the memo of your check.
+            </p>
+          </section>
+          <section>
+            <h3>INTERNATIONAL DONATIONS:</h3>
+            <p>
+              Only donations from within the U.S. are tax-deductible. For USD money orders, please
+              collect donations in your currency, then mail a USD money order to:
+            </p>
+            <strong>Of Home, Family and Future, Inc.</strong><br />
+            9 East 8th Street, Suite 135<br />
+            New York, NY 10003<br />
+          </section>
+          <section>
+            <h3>MONEY ORDERS:</h3>
+            <p>
+              Should be made payable to Of Home, Family and Future. At this time, we don’t accept
+              foreign checks; however, international banks can and will make checks out in USD for
+              you, if you go in and ask. You can then mail us that check, using our address listed above.
+            </p>
+          </section>
+          <section>
+            <h3>TAX-DEDUCTIBILITY:</h3>
+            <p>
+              Of Home, Family and Future is a registered 501(c)(3) non-profit, so all donations made
+              directly to the organization are tax-deductible. Our audited financials as well as our
+              501(c)(3) exemption can be found online here. Our 501(c)(3) document may not be
+              provided to any sponsors or donors on our behalf without our written consent. If
+              you’re looking for our 990 information, please email <a href="">info@ofhome.org</a>
+            </p>
+          </section>
+          <section>
+            <h3>TAX RECEIPTS:</h3>
+            <p>
+              Receipts for online donations may take up to 24 hours to process and email to you. Be
+              on the lookout for yours; tax receipt emails sometimes find their way into junk mail
+              folders. If you don’t get your receipt within 12 hours, please let us know and we’ll gladly
+              reissue you another one. Offline donors receive a formal tax receipt in the mail within
+              four weeks after their donation has been processed. Again, if you don’t get your receipt
+              within that time, just let us know and we’ll resend it.
+            </p>
+          </section>
+        </article>
+      </li>
+      <li>
+        <h3>DO YOU ACCEPT DONATIONS FROM A COMPANY’S “MATCHING GIFT” PROGRAM?</h3>
+        <article>
+          Yes, we absolutely accept matching gifts and are happy to complete the “matching gifts” form.
+        </article>
+      </li>
+      <li>
+        <h3>WHERE DOES MY MONEY GO?</h3>
+        <article>
+          <p>
+            If you donate to the Domestic Violence Initiative, this is what your money goes towards:
+            childcare and summercamp for the kids, cellphone and plan, computer and internet access,
+            education, job training and transportation for the moms.
+          </p>
+          <p>
+            If you donate to the Educational Mentoring Program, this is what your money goes
+            towards: Computer and internet access, books and other college course-related
+            material such as special computer programs, tutoring, transportation home, cell phone
+            and cell phone plan, counseling.
+          </p>
+          <p>
+            If you want your donation to go where it is most needed at the time, don't designate either program.
+          </p>
+        </article>
+      </li>
+      <li>
+        <h3>HOW DO I GET INVOLVED?</h3>
+        <article>
+          We're always looking for mentors for our students and volunteers who can offer their time to help fundraise, raise awareness, and add content to our site. Interested? Sign up here.
+        </article>
+      </li>
+      <li>
+        <h3>DO YOU HAVE PROGRAMS NATIONWIDE OR LOCAL?</h3>
+        <article>
+          Our programs are local: Families and students typically living in New York City, and students attending colleges within New York State.
+        </article>
+      </li>
+      <li>
+        <h3>WHO FUNDS YOU?</h3>
+        <article>
+          <ul>
+            <li>The Harry and Carol Goodman Estate</li>
+            <li>Over the years, our grantors and corporate contributors have included:</li>
+            <li>Hedge Funds Care</li>
+            <li>Macy’s Foundation</li>
+            <li>Maple Row Partners</li>
+            <li>SHG Planning</li>
+            <li>Seymour Science, LLC</li>
+            <li>Alexander E & O Group, LLC</li>
+            <li>Greenberg, Trager, & Herbst, LLP</li>
+            <li>Rothbard & Sinchuk, LLC</li>
+            <li>Tannenbaum, Helpern, Syracuse & Hirschtritt, LLP</li>
+            <li>Fidelity Charitable Gift Fund</li>
+            <li>Vanguard Charitable Endowment</li>
+            <li>GEMS Investment Research</li>
+            <li>Virginia B. Toulmin Foundation</li>
+            <li>Allstate Foundation</li>
+            <li>Avon Products Foundation</li>
+          </ul>
+        </article>
+      </li>
+    </ol>
+  </div>
+<?php get_footer(); ?>
