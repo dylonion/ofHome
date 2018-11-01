05b6637928af461690e58ca00ca3d900721b6316
Dylan Moylan <dylanmoylan1@gmail.com>
2018-11-01 02:10:53 -0400

    Adds new icons for DVI page, and some new lower-rez header images. Adds privacy policy php page, and some header formatting changes.

---
+++ privacy.php
@@ -0,0 +1,103 @@
+<?php /* Template Name: Privacy Policy */ ?>
+<?php get_header(); ?>
+<div class="content">
+  <article class="privacy-policy">
+    <p>
+      Thank you for visiting the Of Home, Family and Future, Inc. (“OHFF”) web site.
+      Our website is owned and operated by Of Home, Family and Future, Inc., a New York 501(c)(3) not-for-
+      profit organization. OHFF is committed to protecting your privacy and the personal information you
+      provide when you visit http://www.ofhome.org (the “Site”). This Privacy Policy explains how we collect,
+      protect and use the information that you voluntarily provide to us through various activities on our Site.
+    </p>
+    <h3>Why We Collect and Use Personal Information</h3>
+    <p>
+      We may collect personal information from individuals at our site when they provide it to us voluntarily
+and knowingly for a specific purpose. This personal information includes, but is not limited to, your name,
+postal address, telephone and fax number and email address. OHFF may collect this information when
+visitors to the Site make online charitable contributions to support OHFF, send correspondence to OHFF
+through the Site, or participate in an activity that requires your personal information. The personal
+information may be used by OHFF, for internal purposes, to analyze and improve the operation of the
+site.<br>
+OHFF does not require that you register or provide personal information to us in order to access and
+browse the Site. If you choose not to provide that information, you will be able to browse our Site, but we
+may not be able to fulfill your request or complete your transaction.
+    </p>
+    <h3>Sharing Information</h3>
+    <p>
+      As a matter of policy, we do not sell, rent, or share personal information provided to us online to third
+      parties. We will only share the information we have collected about you with third parties when (i) the
+      person submitting the information authorizes us to share the information, (ii) it is necessary for the
+      operation of the site to share the information, for example to process a credit card transaction, or (iii)
+      where such release is required by law, or requested by court order, subpoena, or other legal process, or
+      (iv) to assist in the discovery of valid claims, illegal activities, to prevent harm to you, or any other person,
+      or to protect the organization.
+    </p>
+    <h3>Use of Cookies</h3>
+    <p>
+      Our Site may collect non-personal information and data automatically through the use of “cookies.”
+      “Cookies” are small text files that a Web site can use to track repeat visitors, facilitate the visitor’s
+      continuing access and use of the site, permit a site to follow usage behavior and compile aggregate data
+      that will allow site content improvements. If you do not want information collected through the use of
+      cookies, there is a procedure in most browsers that will allow you to deny the cookie feature.
+    </p>
+    <h3>Hyperlinks</h3>
+    <p>
+      This Privacy Policy applies only to the use of the OHFF Site. This Site is linked to other sites and other
+      sites may “link” to this Site. Web sites linking with our Site may have different privacy policies from the
+      one described herein. OHFF is not responsible for the content of the linked web sites, their privacy
+      policies, information collection and information sharing policies and procedures. Please read the privacy
+      policies of all linked sites to understand how they protect your privacy.
+    </p>
+    <h3>Other Websites</h3>
+    <p>
+      Access to and use of such other websites is at your own risk and subject to any terms of use and privacy
+      policies which govern such websites. By providing such links, OHFF shall not be deemed to endorse,
+      recommend, approve, guarantee or introduce any third parties or their services or products, or any facts,
+      views, advice, information, promotions and/or products found on such websites. Similarly, such other
+      websites should not be deemed to endorse, recommend or approve of this Website, or the views contained
+      hereon. OHFF is not responsible for the content contained on any other websites, or for the failure of any
+      product or service offered for sale or advertised on any such websites or for any damages that may result
+      therefrom. Copyrights in the materials or information on the linked websites are owned by other
+      organizations.
+    </p>
+    <h3>Security</h3>
+    <p>
+      OHFF is concerned about the safety and security of your personal information and credit card
+transactions. We do not carry out any financial transactions on our site. We direct you to the secure
+payment site, PayPal.com to make sure that your financial and associated personal information is as
+secure and private as possible. This site uses SSL encryption software to protect sensitive information
+(such as a credit card number or address). This coding system permits your browser to automatically
+encrypt or scramble data before it is sent to them. PayPal strives to protect the confidentiality of your
+information; however no transmission through the Internet can be guaranteed to be 100% secure.
+    </p>
+    <h3>Children’s Online Privacy Protection Act</h3>
+    <p>
+      In compliance with the Children’s Online Privacy Protection Act of 1998 (“COPPA”), our Site operators
+never knowingly request personal information from anyone under the age of 13, without requesting
+verifiable parental consent. If we conclude that a user of this Site is under the age of 13, we will not retain
+or use his or her personal information.
+    </p>
+    <h3>Opting Out</h3>
+    <p>
+      If you do not want to receive communications from OHFF (eg. emails or postal mailings about OHFF
+      programs), or allow OHFF to use your personal information as described in this Privacy Policy, please
+      inform us by sending an email to <a href="mailto:info@ofhome.org">info@ofhome.org</a>
+    </p>
+    <h3>Changes to this Privacy Policy</h3>
+    <p>
+      OHFF reserves the right to change this Privacy Policy at any time, and without notice. If we change our
+Privacy Policy, the revised Privacy Policy will apply to information collected after the Privacy Policy is
+changed and posted.
+    </p>
+    <h3>Contacting Of Home, Family and Future, Inc. (OHFF):</h3>
+    <p>
+      OHFF works hard to ensure that your experience on our Site is a positive one. If you wish to review or
+change information previously provided to us, communicate with us about this Privacy Policy, the
+practices of OHFF or your dealings with our Site, you may contact us by sending an email
+to <a href="mailto:info@ofhome.org">info@ofhome.org</a>. , by telephone at
+(212) 228-8299, or by mail at Of Home, Family and Future, Inc., 9 East 8th Street, Suite 135, New York,
+New York 10003.
+    </p>
+  </article>
+</div>
+<?php get_footer(); ?>
